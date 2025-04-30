62a98d7d83886733d649b59233ea66ca8b505c56
adam <adammagom00@gmail.com>
2025-04-30 03:29:56 +0300

    dist

--- contact.js
+++ contact.js
@@ -16,6 +16,7 @@ export default function loadContact() {
     mapContainer.id = 'map';
     mapContainer.style.height = '500px';
     mapContainer.style.width = '1100px';
+
     mapContainer.style.marginTop = '150px';
     mapContainer.style.marginLeft = '400px';
 
@@ -30,6 +31,7 @@ export default function loadContact() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+ 
     const marker = L.marker([43.3178, 45.6985]).addTo(map); 
     marker.bindPopup('Грозный, Чеченская Республика').openPopup();
 }
